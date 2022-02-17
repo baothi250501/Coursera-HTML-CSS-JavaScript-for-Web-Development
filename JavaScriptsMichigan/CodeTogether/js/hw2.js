06b45e950e81bad6d335a8d34c4aac9a956a7f1b
Nguyễn Lê Bảo Thi <thinguyenlebao@gmail.com>
2022-02-17 15:42:34 +0700

    Update study Michigan

---
+++ JavaScriptsMichigan/CodeTogether/js/hw2.js
@@ -0,0 +1,11 @@
+function openMe(){
+    x = document.getElementById("demo");
+    //x.style.display = "block";
+    x.className="open"
+}
+
+function closeMe(){
+    x = document.getElementById("demo");
+    //x.style.display = "none";
+    x.className="closed"
+}
